4969c6d03fc573913607ce36acbb8877384731a9
Alex M <Alexm717@hotmail.com>
2022-09-06 16:55:19 -0700

    story 2

--- spec/models/project_spec.rb
+++ spec/models/project_spec.rb
@@ -1,15 +1,27 @@
-require 'rails_helper'
+require "rails_helper"
 
 
-RSpec.describe Project, type: :model do
-  describe "validations" do
-    it {should validate_presence_of :name}
-    it {should validate_presence_of :material}
+RSpec.describe(Project, type: :model) do
+  describe("validations") do
+    it { should(validate_presence_of(:name)) }
+    it { should(validate_presence_of(:material)) }
   end
 
-  describe "relationships" do
-    it {should belong_to :challenge}
-    it {should have_many :contestant_projects}
-    it {should have_many(:contestants).through(:contestant_projects)}
+  describe("relationships") do
+    it { should(belong_to(:challenge)) }
+    it { should(have_many(:contestant_projects)) }
+    it { should(have_many(:contestants).through(:contestant_projects)) }
+  end
+
+  describe("model test") do
+    it("#project name ") do
+      jay = Contestant.create(      name: "Jay McCarroll",       age: 40,       hometown: "LA",       years_of_experience: 13)
+      gretchen = Contestant.create(      name: "Gretchen Jones",       age: 36,       hometown: "NYC",       years_of_experience: 12)
+      recycled_material_challenge = Challenge.create(      theme: "Recycled Material",       project_budget: 1000)
+      news_chic = recycled_material_challenge.projects.create(      name: "News Chic",       material: "Newspaper")
+      ContestantProject.create(      contestant_id: jay.id,       project_id: news_chic.id)
+      ContestantProject.create(      contestant_id: gretchen.id,       project_id: news_chic.id)
+      expect(news_chic.proj_name).to(eq(("News Chic")))
+    end
   end
 end
